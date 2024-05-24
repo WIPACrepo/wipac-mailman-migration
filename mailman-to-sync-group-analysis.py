f695b85a1c85ffbd8358b3089993a8a66cc3717c
Vladimir Brik <22751545+vbrik@users.noreply.github.com>
2024-05-24 14:51:35 -0500

    update mailman-to-sync-group-analysis.py

--- mailman-to-sync-group-analysis.py
+++ mailman-to-sync-group-analysis.py
@@ -3,6 +3,7 @@ import argparse
 import sys
 from pprint import pprint
 
+from attrs import define
 import subprocess
 import asyncio
 import pickle
@@ -12,8 +13,9 @@ import thefuzz.fuzz
 from operator import itemgetter
 from krs.groups import get_group_membership
 from enum import *
+import re
 
-user_by_email_manual = {
+MANUAL_EMAIL_MAP = {
     # It looks like this person has 2 accounts: sin and jin.
     # They are probably using jin, and that's the account that has
     # authors_email set to sin@icecube.wisc.edu
@@ -39,12 +41,26 @@ user_by_email_manual = {
     'vandenbrouck@wisc.edu': 'justin',
     'vladimir.brik@icecube.wisc.edu': 'vbrik',
     'xu.zhai@icecube.wisc.edu': 'xuzhai',
+    'mcpreston@icecube.wisc.edu': 'mcpreston',
+    'carlos.pobes.guest@usap.gov': 'cpobes',
+    'collaborationmeetings@icecube.wisc.edu': None,
+    'efriedman09@gmail.com': 'efriedman',
+    'i3runcoord@googlemail.com': None,
+    'ralf.auer.guest@usap.gov': 'rauer',
+    'zsuzsa@astro.columbia.edu': 'zmarka',
 }
 
-known_unknowns = [
+KNOWN_UNKNOWNS = [
     'mtakahashi@chiba-u.jp',
     'vxw@capella2.gsfc.nasa.gov'
 ]
+
+
+def get_sub_addr(user):
+    user_attrs: dict = user['attributes']
+    return (user_attrs.get('mailing_list_email') or user_attrs.get('canonical_email')
+            or f"{user['username']}@icecube.wisc.edu").lower()
+
 
 def find_user(all_users, email):
     ret = []
@@ -53,70 +69,67 @@ def find_user(all_users, email):
         emails = list(filter(None, {userinfo.get('email'), attrs.get('canonical_email'),
                                     attrs.get('mailing_list_email'), attrs.get('author_email'),
                                     f"{username}@icecube.wisc.edu"}))
-        if email in emails:
+        if email.lower() in [addr.strip().lower() for addr in emails]:
             ret.append(userinfo['username'])
     return ret
 
 
-def fuzzy_find(all_users, email, ratio):
-    ret = []
+@define
+class FuzzyMatch:
+    target: str
+    result: str
+    ratio: int
+    data: tuple
+
+def fuzzy_find(all_users, email):
+    best_ratio = -1
+    fuzzy_matches = []
     for username, userinfo in all_users.items():
         attrs = userinfo.get('attributes', {})
-        emails = list(filter(None, {userinfo.get('email'), attrs.get('canonical_email'),
-                                    attrs.get('mailing_list_email'), attrs.get('author_email'),
-                                    f"{username}@icecube.wisc.edu"}))
-        local_parts = [em.split('@')[0] for em in emails if em and em.split('@')[0]]
-        local_part = email.split('@')[0]
-        full_name = f"{userinfo.get('firstName')} {userinfo.get('lastName')}"
-        slack = attrs.get('slack')
-        github = attrs.get('github')
-        author_name = attrs.get('author_name')
-        author_first = attrs.get('author_firstName')
-        author_last = attrs.get('author_lastName')
-        ratios = [thefuzz.fuzz.ratio(local_part, lp)
-                  for lp in local_parts + [full_name, slack, github, author_name, ]]
-        if any(r > ratio for r in ratios):
-            ret.append([username, full_name, emails, slack, ratios])
-    return ret
+        candidate_emails = [userinfo.get('email'), attrs.get('canonical_email'),
+                            attrs.get('mailing_list_email'), attrs.get('author_email'),
+                            f"{username}@icecube.wisc.edu"]
+        candidate_local_parts = [em.split('@')[0] for em in candidate_emails
+                                 if em and em.split('@')[0]]
+        candidate_misc = [f"{userinfo.get('firstName')} {userinfo.get('lastName')}",
+                          attrs.get('slack'), attrs.get('author_name'), attrs.get('github')]
+        candidates = filter(None, candidate_local_parts + candidate_misc)
+        candidates = list(set(str_.strip().lower() for str_ in candidates))
+        local_part = email.split('@')[0].lower()
+        ratios = [thefuzz.fuzz.ratio(local_part, candidate) for candidate in candidates]
+        current_best_ratio = max(ratios)
+        match_descr = (current_best_ratio, [username, candidates, ratios])
+        match = FuzzyMatch(target=email, result=username, ratio=current_best_ratio, data=match_descr)
+        if current_best_ratio > best_ratio:
+            best_ratio = current_best_ratio
+            fuzzy_matches = [match]
+        elif current_best_ratio == best_ratio:
+            fuzzy_matches.append(match)
+    return fuzzy_matches
 
 
-def get_mailing_list_sub_addr(user):
-    user_attrs: dict = user['attributes']
-    return (user_attrs.get('mailing_list_email') or user_attrs.get('canonical_email')
-            or f"{user['username']}@icecube.wisc.edu")
-
-
-def build_email_mappings(mm_emails, all_users, skip, known_mappings):
-    email_mapping = []
+def build_email_mappings(mm_emails, all_users, known_mappings):
+    ambiguous_fuzzy = []
+    email_mapping = {}
     for email in [e for e in mm_emails]:
-        if email in known_unknowns:
-            print(f'SKIPPING {email}')
-            continue
-        for s in skip:
-            if s in email:
-                continue
+        assert email not in email_mapping
         if email in known_mappings:
-            email_mapping.append((email, known_mappings[email]))
+            email_mapping[email] = known_mappings[email]
             continue
-
         matches = find_user(all_users, email)
-        if len(matches) == 2:
-            if [u for u in matches if u.endswith('-local')]:
-                email_mapping.append((email, [u for u in matches if not u.endswith('-local')][0]))
-                continue
-        assert len(matches) < 2
-        if matches:
-            email_mapping.append((email, matches[0]))
+        if len(matches) == 1:
+            email_mapping[email] = matches[0]
+        elif len(matches) == 0:
+            matches = fuzzy_find(all_users, email)
+            if len(matches) == 1 and matches[0].ratio >= 85:
+                email_mapping[email] = matches[0].result
+            else:
+                #print("ambiguous fuzzy match", matches, file=sys.stderr)
+                ambiguous_fuzzy.append((email, matches))
         else:
-            fuzzy_matches = fuzzy_find(all_users, email, 70)
-            assert fuzzy_matches
-            if len(fuzzy_matches) > 1:
-                fuzzy_matches = fuzzy_find(all_users, email, 90)
-                if len(fuzzy_matches) > 1:
-                    fuzzy_matches = fuzzy_find(all_users, email, 99)
-                    assert len(fuzzy_matches) == 1
-            email_mapping.append((email, fuzzy_matches[0][0]))
-    return dict(email_mapping)
+            raise ValueError("Ambiguous exact match", matches)
+            ambiguous_exact.append((email, matches))
+    return dict(email_mapping), ambiguous_fuzzy
 
 
 async def main():
@@ -129,40 +142,61 @@ async def main():
     parser.add_argument('--all-users', required=True)
     args = parser.parse_args()
 
-    kc = get_rest_client()
     try:
         with open(args.all_users, 'rb') as f:
             all_users = pickle.load(f)
     except FileNotFoundError:
+        kc = get_rest_client()
         all_users = await list_users(rest_client=kc)
         with open(args.all_users, 'wb') as f:
             pickle.dump(all_users, f)
 
-    mm_emails = subprocess.check_output(
-        ['ssh', 'mailman', '/usr/lib/mailman/bin/list_members', args.list],
-        encoding='utf').split()
+    subprocess.check_output(['ssh', 'mailman', './pickle-mailman-list.py', '--list', args.list])
+    subprocess.check_output(['scp', f'mailman:{args.list}.pkl', '.'])
+    list_cfg = pickle.load(open(f"{args.list}.pkl", 'rb'))
+    email_regex = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+    mm_emails = list_cfg['digest_members'] + list_cfg['regular_members'] + \
+        [str_ for str_ in list_cfg['accept_these_nonmembers']
+         if re.match(email_regex, str_)]
 
-    for email in known_unknowns:
+
+    #mm_emails = subprocess.check_output(
+    #    ['ssh', 'mailman', '/usr/lib/mailman/bin/list_members', args.list],
+    #    encoding='utf').split()
+    mm_emails = sorted(set(email.strip().lower() for email in mm_emails))
+
+    for email in KNOWN_UNKNOWNS + args.skip:
         mm_emails.remove(email)
 
-    usernames_by_mm_email = build_email_mappings(mm_emails, all_users, args.skip, user_by_email_manual)
+    mm_email_map, ambiguous_fuzzy = build_email_mappings(mm_emails, all_users, MANUAL_EMAIL_MAP)
+    #for email, username in sorted(mm_email_map.items()):
+    #    print('MAP', email, username)
+    for row in ambiguous_fuzzy:
+        print('FAILED_TO_MAP', row[1])
 
-    #print('\n'.join(' '.join(e) for e in sorted(usernames_by_mm_email, key=itemgetter(1))))
+    return
 
-    kc_group_usernames = await get_group_membership(args.group, rest_client=kc)
-    effective_addrs_for_lists = dict((username, get_mailing_list_sub_addr(all_users[username]))
-                     for username in kc_group_usernames)
+    kc = get_rest_client()
+    group_usernames = await get_group_membership(args.group, rest_client=kc)
+    username_sub_addr = dict((username, get_sub_addr(all_users[username]))
+                             for username in group_usernames)
 
-    mm_usernames_not_in_group = [usernames_by_mm_email[email] for email in mm_emails
-                    if usernames_by_mm_email[email] not in kc_group_usernames]
+    mm_usernames_not_in_group = set(mm_email_map[email] for email in mm_emails
+                                 if mm_email_map[email] not in group_usernames)
     mm_emails_in_group = [email for email in mm_emails
-                    if usernames_by_mm_email[email] in kc_group_usernames]
-    need_addr_change = [(usernames_by_mm_email[email], email, effective_addrs_for_lists[usernames_by_mm_email[email]])
-                   for email in mm_emails_in_group
-                   if email != effective_addrs_for_lists[usernames_by_mm_email[email]]]
+                          if mm_email_map[email] in group_usernames]
+    need_addr_change = [(mm_email_map[email], email, username_sub_addr[mm_email_map[email]])
+                        for email in mm_emails_in_group
+                        if (
+                                email != username_sub_addr[mm_email_map[email]]
+                                and not
+                                (email.endswith('@icecube.wisc.edu')
+                                 and username_sub_addr[mm_email_map[email]].endswith('@icecube.wisc.edu')))]
 
-    pprint(need_addr_change)
-    pprint(mm_usernames_not_in_group)
+    for line in sorted(need_addr_change):
+        print('ADDR_CHANGE', line)
+    for line in sorted(mm_usernames_not_in_group):
+        print('NOT_IN_GROUP', line)
 
 if __name__ == '__main__':
     sys.exit(asyncio.run(main()))
