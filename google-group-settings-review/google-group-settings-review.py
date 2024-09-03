d9236973316638eaf2999df15a78c2607a8992d0
Vladimir Brik <22751545+vbrik@users.noreply.github.com>
2024-09-03 14:24:42 -0500

    update google-group-settings-review.py

--- google-group-settings-review/google-group-settings-review.py
+++ google-group-settings-review/google-group-settings-review.py
@@ -12,7 +12,6 @@ def main():
             formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('args', nargs='*')
     args = parser.parse_args()
-    pprint(args)
 
     info = check_output("/usr/local/bin/gam/gam info group icb@icecube.wisc.edu".split(), encoding="utf8")
     info_lines = [l.strip() for l in info.split("\n") if l.strip()]
@@ -21,30 +20,92 @@ def main():
 
     basic_attrs = ('name', 'description', 'email')
 
-    posting_policy = (
-        ('whoCanPostMessage', "managers, members, owners, domain (IceCube), anyone"),
-        ('messageModerationLevel', "everybody, non-members, new-members, none/nobody"),
-        ('spamModerationLevel', "allow, moderate, reject"),
-        ('whoCanModerateContent', "members, managers"),
-    )
-    sendMessageDenyNotification
+    adv_attrs = {
+        # posting policy
+        'whoCanPostMessage': {
+            None: "Who can post",
+            "ANYONE_CAN_POST": "anybody (all messages are accepted)",
+            "ALL_MANAGERS_CAN_POST": "group managers",
+            "ALL_MEMBERS_CAN_POST": "group members",
+            "ALL_IN_DOMAIN_CAN_POST": "everybody from IceCube",
+        },
+        "messageModerationLevel": {
+            None: "Message moderation",
+            "MODERATE_ALL_MESSAGES": "all messages are moderated",
+            "MODERATE_NON_MEMBERS": "messages from non-members are moderated",
+            "MODERATE_NONE": "accept all messages without moderation",
+        },
+        "spamModerationLevel": {
+            None: "Spam policy",
+            "ALLOW": "accept suspected spam",
+            "MODERATE": "moderate suspected spam",
+            "REJECT": "reject suspected spam",
+        },
+        "whoCanModerateContent": {
+            None: "Who can accept/reject messages",
+            "ALL_MEMBERS": "all group members",
+            "OWNERS_AND_MANAGERS": "group managers",
+            "OWNERS_ONLY": "group owners",
+            "NONE": "nobody",
+        },
+        "sendMessageDenyNotification": {
+            None: "When non-spam is rejected",
+            "true": "notify senders",
+            "false": "reject silently",
+        },
+        # privacy
+        "whoCanViewGroup": {
+            None: "Who can view message archive",
+            "ANYONE_CAN_VIEW": "anybody (publicly available on the Internet)",
+            "ALL_IN_DOMAIN_CAN_VIEW": "anybody from IceCube",
+            "ALL_MEMBERS_CAN_VIEW": "only group member",
+            "ALL_MANAGERS_CAN_VIEW": "only managers",
+        },
+        "whoCanViewMembership": {
+            None: "Who can view group membership",
+            "ALL_IN_DOMAIN_CAN_VIEW": "everybody from IceCube",
+            "ALL_MEMBERS_CAN_VIEW": "only group members",
+            "ALL_MANAGERS_CAN_VIEW": "only group managers",
 
-    privacy = (
-        ('whoCanViewGroup', "anyone (bad!), domain (IceCube), members, managers"),
-        ('whoCanViewMembership', "domain (IceCube), members, managers"),
-        ('whoCanDiscoverGroup', "anyone, domain (IceCube), members"),
-        ('isArchived', "true, false"),
-    )
-
-    member_management = (
-        ('whoCanAdd', "members, managers"),
-        ('whoCanJoin', "domain (IceCube), domain-can-request, anybody-by-invite"),
-        ('whoCanLeaveGroup', "members, nobody"),
-    )
+        },
+        "whoCanDiscoverGroup": {
+            None: "Who can find this group",
+            "ANYONE_CAN_DISCOVER": "anybody on the Internet",
+            "ALL_IN_DOMAIN_CAN_DISCOVER": "everybody from IceCube",
+            "ALL_MEMBERS_CAN_DISCOVER": "only members",
+        },
+        "isArchived": {
+            None: "Maintain archive of group messages",
+            "true": "yes",
+            "false": "no",
+        },
+        # membership
+        "whoCanModerateMembers": {
+            None: "Who can add/remove members",
+            "ALL_MEMBERS": "group members",
+            "OWNERS_AND_MANAGERS": "group owners and managers",
+            "OWNERS_ONLY": "group owners",
+            "NONE": "nobody (managed out of band)",
+        },
+        "whoCanJoin": {
+            None: "Who can join the group",
+            "ANYONE_CAN_JOIN": "anybody on the internet can join",
+            "ALL_IN_DOMAIN_CAN_JOIN": "everybody from IceCube can join",
+            "INVITED_CAN_JOIN": "only invited users can join",
+            "CAN_REQUEST_TO_JOIN": "non-members can request to join",
+        },
+        "whoCanLeaveGroup": {
+            None: "Who can leave the group",
+            "ALL_MANAGERS_CAN_LEAVE": "only managers",
+            "ALL_MEMBERS_CAN_LEAVE": "any member",
+            "NONE_CAN_LEAVE": "nobody",
+        },
+    }
 
     patches = (
         ("isArchived", "isArchived (keep message archives)"),
         ("whoCanModerateContent", "whoCanModerateContent (messages)"),
+        ("whoCanModerateMembers", "xxxx (messages)"),
         ('messageModerationLevel', "messageModerationLevel (whom to moderate)"),
         ('whoCanViewGroup (access to archives)', "anyone (bad!), domain (IceCube), members, managers"),
     )
@@ -55,22 +116,24 @@ def main():
 
     def show_attrs(attrs):
         text = ""
-        for attr, choices in attrs:
-            text += [line for line in info_lines if line.startswith(f"{attr}:")][0]
-            text += f"  {'{'}{choices}{'}'}" + "\n"
+        for attr in attrs:
+            val = [line.strip().split() for line in info_lines
+                   if line.startswith(f"{attr}:")][0][-1]
+            text += f"{adv_attrs[attr][None]}: {adv_attrs[attr][val]}\n"
         return text
 
     message += "\nPOSTING POLICY\n"
-    message += show_attrs(posting_policy)
+    message += show_attrs(("whoCanPostMessage", "messageModerationLevel", "spamModerationLevel",
+                           "whoCanModerateContent", "sendMessageDenyNotification"))
 
     message += "\nPRIVACY SETTINGS\n"
-    message += show_attrs(privacy)
+    message += show_attrs(("isArchived", "whoCanViewGroup", "whoCanViewMembership", "whoCanDiscoverGroup"))
 
     message += "\nMEMBER MANAGEMENT\n"
-    message += show_attrs(member_management)
+    message += show_attrs(("whoCanModerateMembers", "whoCanJoin", "whoCanLeaveGroup"))
 
-#    message += "\nMEMBER LIST\n"
-#    message += "\n".join(members)
+    message += "\nMEMBER LIST\n"
+    #message += "\n".join(members)
 
     for old, new in patches:
         message = message.replace(old, new)
